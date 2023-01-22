c856cf8a2da31c790dcc0d68bccc9e8cb7bc3169
rachid chakor <chakor.rachid.1@gmail.com>
2023-01-22 15:30:33 -0500

    created the first racket

--- myApp.py
+++ myApp.py
@@ -1,4 +1,6 @@
 import turtle
+import racket1
+
 
 myScreen = turtle.Screen()
 myScreen.title("Ping Pong by Rachid")
@@ -6,5 +8,9 @@ myScreen.bgcolor("black")
 myScreen.setup(width=800, height=800)
 myScreen.tracer(0)
 
+#racket1
+racket1
+
+#main game loop
 while True:
     myScreen.update()
---
+++ racket1.py
@@ -0,0 +1,9 @@
+import turtle
+
+racket1 = turtle.Turtle()
+racket1.speed(0)
+racket1.shape("square")
+racket1.color("green")
+racket1.shapesize(stretch_wid=5, stretch_len=1)
+racket1.penup()
+racket1.goto(-350,0)
