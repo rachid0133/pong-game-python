8e52e5d8c5962dcc527ad21c37e871a2a93d8f40
rachid chakor <chakor.rachid.1@gmail.com>
2023-01-22 13:01:47 -0500

    created myScreen

---
+++ myApp.py
@@ -0,0 +1,10 @@
+import turtle
+
+myScreen = turtle.Screen()
+myScreen.title("Ping Pong by Rachid")
+myScreen.bgcolor("black")
+myScreen.setup(width=800, height=800)
+myScreen.tracer(0)
+
+while True:
+    myScreen.update()
